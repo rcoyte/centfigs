df7c5791093f8657fc24dbe824c53e14c79d4d31
rcoyte <77900811+rcoyte@users.noreply.github.com>
2021-01-31 20:53:31 -0500

    Now you can adjust the percentiles, and the box colors match the marker colors. Also added commentary for the user

--- boxes.py
+++ boxes.py
@@ -4,25 +4,33 @@ Created on Sun Jan 31 18:44:53 2021
 
 @author: rmc33
 """
-
-
+###############################################################################
+#THESE ARE THE THINGS YOU NEED TO EDIT TO MAKE THIS CODE WORK
+###############################################################################
+filename = 'data.xlsx' #YOUR FILE NAME
+x='x'
+y='y'
+ylab= 'U ($\mu$g/L)' #WHAT THE Y LABEL WILL SAY
+xlab= 'HCO$_{3}^{-}$ (mg/L)' #WHAT THE X LABEL WILL SAY
+category = 'Cat_1' #THE CATEOGRIES YOU ARE BREAKING YOUR DATA UP INTO
+perc_hi = 90 #THE TOP PERCENTILE
+perc_lo = 10 #THE BOTTOM PERCENTILE
+###############################################################################
+#IF YOU WANT TO CHANGE THE COLORS ASK ME HOW TO DO THE PALETTE BELOW
+###############################################################################
+pal = ['#F7931D', '#00B9F1', '#00A875', 'ECDE38', '#F15A22', '#00000', 
+       '#0072BC', '#DA6FAB']  
+###############################################################################
+#DONT MESS WITH ANYTHING BELOW THIS LINE. SERIOUSLY. 
+###############################################################################
 import os
 import pandas as pd
 import numpy as np
-from scipy import stats
 import matplotlib.pyplot as plt
-import statistics
-from scipy.stats import kruskal
-from scipy.stats import spearmanr
 import seaborn as sns
-import scikit_posthocs as sp
-import string
 
-PJf = os.path.join('data.csv')
-df = pd.read_csv(PJf, 
-                     na_values=['OVER', 'nd', 'bdl','n.d.', 'N.D.', 'n.a.', 
-                                'n/a','OR','#VALUE','ClO','J', 'censor'])
-                                
+
+
 SMALL_SIZE = 10
 MEDIUM_SIZE = 16
 BIGGER_SIZE = 20
@@ -40,18 +48,50 @@ plt.rc('ytick', labelsize= BIGGER_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize= MEDIUM_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=MEDIUM_SIZE)  # fontsize of the figure title
 
-pal = ["#f5f7a3", '#72b555', '#0f4269', '#a8f3ff', '#038499', '#dfadff', 
-       '#5f0399','#f79999', '#910303']
-       
+
+filetype = filename.split('.')[-1]
+PJf = os.path.join(filename)
+if filetype == 'csv':
+    df = pd.read_csv(PJf, 
+                     na_values=['OVER', 'nd', 'bdl','n.d.', 'N.D.', 'n.a.', 
+                                'n/a','OR','#VALUE','ClO','J', 'censor'])
+elif filetype == 'xlsx':
+    df = pd.read_excel(PJf, 
+                     na_values=['OVER', 'nd', 'bdl','n.d.', 'N.D.', 'n.a.', 
+                                'n/a','OR','#VALUE','ClO','J', 'censor'])
+else: 
+    print('ERROR: INPUT FILE MUST BE CSV OR XLSX')
+
+
+
+
+cats = df[category].unique()
+
+  
+k=len(cats)
+del pal[k:]
+
+
 fig,ax=plt.subplots(figsize=(5,5))
-x='x'
-y='y'
-ylab= 'U ($\mu$g/L)'
-xlab= 'HCO$_{3}^{-}$ (mg/L)'
-f=sns.scatterplot(x, y, data=df, hue='Cat_2', 
-                 edgecolor='k', s=150)
+f=sns.scatterplot(x, y, data=df, hue=category, 
+                 edgecolor='k', s=150, palette =  pal)
 plt.xlabel(xlab)
 plt.ylabel(ylab)
+for indexi, i in enumerate(cats):
+    x25 = np.percentile(df[df[category]== i][x], perc_lo)
+    x75 = np.percentile(df[df[category]== i][x], perc_hi)
+    y25 = np.percentile(df[df[category]== i][y], perc_lo)
+    y75 = np.percentile(df[df[category]== i][y], perc_hi)
+    left = x25
+    bottom=y25
+    width=x75-x25
+    height=y75-y25
+    rect = plt.Rectangle((left, bottom), width, height,
+                     fill=False)
+    ax.add_patch(rect)
+    rect = plt.Rectangle((left, bottom), width, height,
+                     facecolor=pal[indexi], alpha=0.1)
+    ax.add_patch(rect)
 ax.legend(bbox_to_anchor=(1.05, 1), loc=2)
 #ax.get_legend().remove()
 plt.show() 
